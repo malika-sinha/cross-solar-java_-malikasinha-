038f92a43f1a6f866fd1e085faae628f5fdaa95f
unknown <malika.sinha29@gmail.com>
2018-08-12 03:04:06 +0530

    First commit

---
+++ CrossOverApplication/src/main/java/com/crossover/techtrial/repository/PanelRepository.java
@@ -0,0 +1,18 @@
+package com.crossover.techtrial.repository;
+
+import com.crossover.techtrial.model.Panel;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+/**
+ * PanelRepository allows all operations to Panel Entity.
+ * @author Crossover
+ *
+ */
+
+@RestResource(exported = false)
+public interface PanelRepository extends PagingAndSortingRepository<Panel, Long> {
+  Panel findBySerial(String serial);
+  
+  //Method added here which was not available #Fixing defect
+  void save(panel);
+}
